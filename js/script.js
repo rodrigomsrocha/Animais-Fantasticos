18b2b5feb342e950f047b730ca9064eec4ed9022
Rodrigo Rocha <rodrigomtc82@gmail.com>
2021-03-13 02:46:38 -0300

    refactored funcionamento

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from "./modules/init-modal.js";
 import Tooltip from "./modules/tooltip.js";
 import Dropdown from "./modules/dropdown-menu.js";
 import MenuMobile from "./modules/menu-mobile.js";
-import initFuncionamento from "./modules/funcionamento.js";
+import Funcionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
 import ScrollAnima from "./modules/scroll-anima.js";
@@ -45,7 +45,8 @@ const menuMobile = new MenuMobile(
 );
 menuMobile.init();
 
-initFuncionamento();
+const funcionamento = new Funcionamento("[data-semana]", "aberto");
+funcionamento.init();
 
 fetchAnimais("../animais.json", ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
